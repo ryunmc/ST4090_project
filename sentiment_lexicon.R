bc5f2ef324ee586a291837ac39cd53c853b17d23
ryan <ryunmc@gmail.com>
2014-11-17 11:24:46 +0000

    haha

---
+++ sentiment_lexicon.R
@@ -0,0 +1,4 @@
+#loads the positive and negative lexicon
+
+hu.liu.pos = scan('C:/Users/Ryanm/Desktop/College/sentiment_lexicon/positive-words.txt', what='character', comment.char=';')
+hu.liu.neg = scan('C:/Users/Ryanm/Desktop/College/sentiment_lexicon/negative-words.txt', what='character', comment.char=';')
